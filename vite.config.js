4f935ed43f3ac0db6bfb43a9523e57a398083e08
YasmeenMahmoud <153022052+yasminMahmoud16@users.noreply.github.com>
2025-01-31 20:15:29 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // base for github not vercle
-  base:"/E-commerceApp/"
+  // base:"/E-commerceApp/"
 })
